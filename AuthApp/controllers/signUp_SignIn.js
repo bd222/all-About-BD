b0959a6c306c040bf6f84b862deca66bf28dfbbd
Debapriyo Das <debapriyo00@gmail.com>
2024-02-02 21:06:18 +0530

    Complete Auth App  but Problem while Get Token form cookie.

--- AuthApp/controllers/signUp_SignIn.js
+++ AuthApp/controllers/signUp_SignIn.js
@@ -1,12 +1,14 @@
 const User = require('../models/user')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 
 exports.signUp = async(req, res)=>{
     try {
         const {name, email, password, role} = req.body;
-        const CheckUser = await User.findOne({email})
-        if(CheckUser){
+        const user = await User.findOne({email})
+        if(user){
             return res.status(400).json({
                 success:false, 
                 message:`User is Already Exists!`
@@ -42,5 +44,66 @@ exports.signUp = async(req, res)=>{
             success:false, 
             message:`There are some Problem While create User!`
         })
+    }
+}
+
+
+
+
+//LogIn.
+exports.logIn = async(req, res)=>{
+    try {
+        const {email, password} = req.body
+        let user = await User.findOne({email})
+        if(!user){
+            return res.status(400).json({
+                success:false, 
+                message:`Go and Resgester then You Log In!!`
+            })
+        };
+
+        if(bcrypt.compare(password, user.password)){
+            //jodi password match kore jai.
+            //token generate korbo.
+            //For generating Token need 3 things first is Payload , JWT_SCRECT_KEY
+            //Create payload .
+            const payload = {
+                email:user.email, 
+                id: user._id, 
+                role:user.role
+            }
+            let token = jwt.sign(payload,process.env.JWT_SECRECT_KEY, {
+                expiresIn:"2h"
+            })
+            user = user.toObject()
+            user.token = token //token ta k cookie vetor add korar jonno.
+            user.password = undefined
+
+            const option = {
+                expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000), 
+                httpOnly:true, 
+            }
+
+            //now cookie maddhame res send korbo.
+            res.cookie("Cookie", token, option).status(200).json({
+                success:true, 
+                token, 
+                user, 
+                message:`User logIn Successfully.`
+            })
+
+        }else{
+            //Password Not Match.
+            return res.status(403).json({
+                success:false,
+                message:"Password Incorrect!",
+            });
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            success:false,
+            message:"Login Failure",
+        });
     }
 }
--- AuthApp/routers/routers.js
+++ AuthApp/routers/routers.js
@@ -2,11 +2,38 @@ const express = require('express')
 const router = express.Router()
 
 
-const { signUp } = require('../controllers/signUp_SignIn')
+const { signUp, logIn } = require('../controllers/signUp_SignIn')
+const { auth, isStudent, isAdmin } = require('../middleware/auth.middleware')
 
 
 
 router.post('/signUp', signUp)
+router.post('/logIn', logIn)
+
+router.get('/test', auth, (req, res)=>{
+    res.json({
+        success:true, 
+        message:`This the test Protcted Route.`
+    })
+})
+
+
+
+router.get('/isStudent', auth, isStudent, (req, res)=>{
+    res.json({
+        success:true, 
+        message:`This the Student Protcted Route.`
+    })
+})
+
+
+
+router.get('/isAdmin', auth, isAdmin, (req, res)=>{
+    res.json({
+        success:true, 
+        message:`This the Admin Protcted Route.`
+    })
+})
 
 
 module.exports = router
